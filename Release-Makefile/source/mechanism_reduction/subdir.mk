a711c8962083f31a41e15cdcd846885557ee993d
DetlevCM <detlevcm@googlemail.com>
2016-01-05 16:25:32 +0100

    typo fix & GCC c++ version change

---
+++ Release-Makefile/source/mechanism_reduction/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../source/mechanism_reduction/Combine_Species-k-based.cpp \
+../source/mechanism_reduction/Compare_Concentrations.cpp \
+../source/mechanism_reduction/Get_Combine_Species_Mapping.cpp \
+../source/mechanism_reduction/Process_Thermodynamics_Species_Classes.cpp \
+../source/mechanism_reduction/Reduce_Reactions_v2.cpp \
+../source/mechanism_reduction/Remove_Species.cpp \
+../source/mechanism_reduction/Rename_Species.cpp \
+../source/mechanism_reduction/Species_Class_Mapping.cpp \
+../source/mechanism_reduction/mechanism_reduction_lumping.cpp 
+
+OBJS += \
+./source/mechanism_reduction/Combine_Species-k-based.o \
+./source/mechanism_reduction/Compare_Concentrations.o \
+./source/mechanism_reduction/Get_Combine_Species_Mapping.o \
+./source/mechanism_reduction/Process_Thermodynamics_Species_Classes.o \
+./source/mechanism_reduction/Reduce_Reactions_v2.o \
+./source/mechanism_reduction/Remove_Species.o \
+./source/mechanism_reduction/Rename_Species.o \
+./source/mechanism_reduction/Species_Class_Mapping.o \
+./source/mechanism_reduction/mechanism_reduction_lumping.o 
+
+CPP_DEPS += \
+./source/mechanism_reduction/Combine_Species-k-based.d \
+./source/mechanism_reduction/Compare_Concentrations.d \
+./source/mechanism_reduction/Get_Combine_Species_Mapping.d \
+./source/mechanism_reduction/Process_Thermodynamics_Species_Classes.d \
+./source/mechanism_reduction/Reduce_Reactions_v2.d \
+./source/mechanism_reduction/Remove_Species.d \
+./source/mechanism_reduction/Rename_Species.d \
+./source/mechanism_reduction/Species_Class_Mapping.d \
+./source/mechanism_reduction/mechanism_reduction_lumping.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source/mechanism_reduction/%.o: ../source/mechanism_reduction/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=gnu++11 -I"X:\workspace\CKS branches\headers" -O2 -m64 -march=native -Wall -c -fmessage-length=0 -v -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Release-Makefile/source/solver_calculations/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../source/solver_calculations/Calculate_Rate_Constants.cpp \
+../source/solver_calculations/Calculate_Reaction_Rates.cpp \
+../source/solver_calculations/Calculate_Thermodynamics.cpp \
+../source/solver_calculations/IntegrateRoutine.cpp \
+../source/solver_calculations/Jacobian.cpp \
+../source/solver_calculations/ODE_RHS_Liquid_Phase.cpp \
+../source/solver_calculations/SpeciesLossRate.cpp 
+
+OBJS += \
+./source/solver_calculations/Calculate_Rate_Constants.o \
+./source/solver_calculations/Calculate_Reaction_Rates.o \
+./source/solver_calculations/Calculate_Thermodynamics.o \
+./source/solver_calculations/IntegrateRoutine.o \
+./source/solver_calculations/Jacobian.o \
+./source/solver_calculations/ODE_RHS_Liquid_Phase.o \
+./source/solver_calculations/SpeciesLossRate.o 
+
+CPP_DEPS += \
+./source/solver_calculations/Calculate_Rate_Constants.d \
+./source/solver_calculations/Calculate_Reaction_Rates.d \
+./source/solver_calculations/Calculate_Thermodynamics.d \
+./source/solver_calculations/IntegrateRoutine.d \
+./source/solver_calculations/Jacobian.d \
+./source/solver_calculations/ODE_RHS_Liquid_Phase.d \
+./source/solver_calculations/SpeciesLossRate.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source/solver_calculations/%.o: ../source/solver_calculations/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=gnu++11 -I"X:\workspace\CKS branches\headers" -O2 -m64 -march=native -Wall -c -fmessage-length=0 -v -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
