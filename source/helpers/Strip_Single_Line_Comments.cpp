b8aeb344f6aab7963c6bb3985c18033d0fda0f68
DetlevCM <detlevcm@googlemail.com>
2018-09-30 19:36:11 +0200

    split helper functions into one function per file

---
+++ source/helpers/Strip_Single_Line_Comments.cpp
@@ -0,0 +1,50 @@
+/*
+ * tokenise_string.cpp
+ *
+ *  Created on: 14 May 2016
+ *      Author: DetlevCM
+ */
+
+#include <Headers.hpp>
+
+string Strip_Single_Line_Comments(string input , vector<string> tokens)
+{
+
+	string output;
+	bool Token_Is_Found;
+	Token_Is_Found = false;
+	string AllTokens;
+
+	int i;
+
+	for(i=0;i<(int)tokens.size();i++)
+	{
+		if(input.find(tokens[i]) != string::npos)
+		{
+			Token_Is_Found = true;
+		}
+		AllTokens = AllTokens+tokens[i];
+	}
+
+	if(Token_Is_Found){
+
+		char *cstr, *p;
+		string str = input;
+		cstr = new char [str.size()+1];
+		strcpy (cstr, str.c_str());
+
+		p=strtok (cstr,AllTokens.c_str());
+		output = p;
+
+		delete[] cstr;
+		delete[] p;
+	}
+	else
+	{
+		output = input;
+	}
+
+	return output;
+}
+
+
---
+++ source/helpers/Test_If_Word_Found.cpp
@@ -0,0 +1,28 @@
+/*
+ * tokenise_string.cpp
+ *
+ *  Created on: 14 May 2016
+ *      Author: DetlevCM
+ */
+
+#include <Headers.hpp>
+
+// from reading on the web, this approach is slow - but for the input file will do
+bool Test_If_Word_Found(string string_to_search, string search_term)
+{
+	// tolower or toupper - either works, just be consistent
+	transform(search_term.begin(),search_term.end(),search_term.begin(), tolower);
+	transform(string_to_search.begin(),string_to_search.end(),string_to_search.begin(), tolower);
+
+	bool output;
+	if(string_to_search.find(search_term)!=string::npos)
+	{
+		output = true;
+	}
+	else
+	{
+		output = false;
+	}
+	return output;
+}
+
