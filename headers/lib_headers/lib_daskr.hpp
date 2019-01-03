0f2f6176224724fd8d7aab34da8a0eae69dbbb5c
DetlevCM <detlevcm@googlemail.com>
2019-01-03 20:32:16 +0100

    create header file and improve formatting for odepack header

--- headers/lib_headers/lib_daskr.hpp
+++ headers/lib_headers/lib_daskr.hpp
@@ -11,6 +11,44 @@
 
 extern "C" {
 
+/*
+C      IMPLICIT DOUBLE PRECISION(A-H,O-Z)
+C      INTEGER NEQ, INFO(N), IDID, LRW, LIW, IWORK(LIW), IPAR(*)
+C      DOUBLE PRECISION T, Y(*), YPRIME(*), TOUT, RTOL(*), ATOL(*),
+C         RWORK(LRW), RPAR(*)
+C      EXTERNAL RES, JAC, PSOL, RT
+C
+C      CALL DDASKR (RES, NEQ, T, Y, YPRIME, TOUT, INFO, RTOL, ATOL,
+C     *             IDID, RWORK, LRW, IWORK, LIW, RPAR, IPAR, JAC, PSOL,
+C     *             RT, NRT, JROOT)
+C
+C  Quantities which may be altered by the code are:
+C     T, Y(*), YPRIME(*), INFO(1), RTOL, ATOL, IDID, RWORK(*), IWORK(*)
+ */
+
+void ddaskr_(
+		void* ,   //RES,
+		int* ,    //NEQ,
+		double* , //T,
+		double* , //Y,
+		double* , //YPRIME,
+		double* , //TOUT,
+		int* ,    //INFO,
+		double* , //RTOL,
+		double* , //ATOL,
+		int* ,    //IDID,
+		double* , //RWORK,
+		int* ,    //LRW,
+		int* ,    //IWORK,
+		int* ,    //LIW,
+		double* , //RPAR,
+		int* ,    //IPAR,
+		void* ,   //JAC,
+		void* ,   //PSOL,
+		void* ,   //RT,
+		int* ,    //NRT,
+		int*      //JROOT
+);
 
 }
 
--- headers/lib_headers/lib_odepack.hpp
+++ headers/lib_headers/lib_odepack.hpp
@@ -12,43 +12,43 @@
 extern "C" {
 
 void dlsode_(
-		void*, //F,
-		int*, //NEQ,
+		void*,   //F,
+		int*,    //NEQ,
 		double*, //Y,
 		double*, //T,
 		double*, //TOUT,
-		int*, //ITOL
+		int*,    //ITOL
 		double*, //RTOL
 		double*, //ATOL
-		int*, //ITASK
-		int*, //ISTATE
-		int*, //IOPT - zero without optional inputs
+		int*,    //ITASK
+		int*,    //ISTATE
+		int*,    //IOPT - zero without optional inputs
 		double*, //RWORK
-		int*, //LRW
-		int*, //IWORK =  20 for MF = 10 OR  20 + NEQ  for MF = 21, 22, 24, or 25.
-		int*, //LIW
-		void* ,//JAC,
-		int* //MF
+		int*,    //LRW
+		int*,    //IWORK =  20 for MF = 10 OR  20 + NEQ  for MF = 21, 22, 24, or 25.
+		int*,    //LIW
+		void* ,  //JAC,
+		int*     //MF
 );
 
 void dlsoda_(
-		void*, // routine that supplied f(x)
-		int*, // NEQ
+		void*,   // routine that supplied f(x)
+		int*,    // NEQ
 		double*, // Y
 		double*, //T
 		double*, //TOUT
-		int*, //ITOL = 1 or 2 according as ATOL (below) is a scalar or array.
+		int*,    //ITOL = 1 or 2 according as ATOL (below) is a scalar or array.
 		double*, //RTOL
 		double*, //ATOL
-		int*, //ITASK
-		int*, //ISTATE
-		int*, //IOPT - zero without optional inputs
+		int*,    //ITASK
+		int*,    //ISTATE
+		int*,    //IOPT - zero without optional inputs
 		double*, //RWORK = real work array of length at least:  22 + NEQ * MAX(16, NEQ + 9) + 3*NG.
-		int*, //LRW
-		int*, //IWORK = integer work array of length at least  20 + NEQ.
-		int*, //LIW
-		void*, //,JDUM - void with Jacobian, use dummy name if none
-		int* //JT - apparently set to 2
+		int*,    //LRW
+		int*,    //IWORK = integer work array of length at least  20 + NEQ.
+		int*,    //LIW
+		void*,   //,JDUM - void with Jacobian, use dummy name if none
+		int*     //JT - apparently set to 2
 );
 
 }
